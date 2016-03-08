5817233ed75d60ef84f50467764332b17a439260
ludovicdannhoff <ludovic.dannhoff@gmail.com>
2016-03-08 15:46:36 +0100

    Modif traitement message

--- models/MessageManager.class.php
+++ models/MessageManager.class.php
@@ -12,22 +12,11 @@ class MessageManager
 
 	public function createMessage($content)
 	{
-		$content = mysqli_real_escape_string($this->db, $content);
+		$message = new Message();
+		$message->setContent($content);
+		$content = mysqli_real_escape_string($this->db, $message->getContent());
 		$query = "INSERT INTO message (content) VALUES('".$content."')";
 		$res = mysqli_query($this->db, $query);
-		if ($res)
-		{
-			$message = mysqli_fetch_object($res, "Message");
-			if ($message)
-			{
-				return $message;
-			}
-			else
-				throw new Exception("La requête SQL n'a pas été exécutée");
-		}
-		else 
-			throw new Exception("Erreur interne");
 	}
-	
 }
 ?>
