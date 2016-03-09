f8b27ca2504af9681f3174505a3d3f67ea9b7813
ludovicdannhoff <ludovic.dannhoff@gmail.com>
2016-03-09 14:11:40 +0100

    affichage des messages

--- apps/display_message.php
+++ apps/display_message.php
@@ -1,1 +1,16 @@
-<?php require('views/display_message.phtml'); ?>
+<?php 
+
+$query = "SELECT id_author, content FROM message";// On déclare une variable qui contiendra notre requête SQL
+$result = mysqli_query($db, $query);// On exécute notre requête SQL
+$number = 1;
+while ($message = mysqli_fetch_assoc($result))// On récupère les résultats de notre requête un par un
+{
+	$id = $message['id_author'];
+	$content = $message['content'];
+	$number++;
+	require('views/display_message.phtml'); 
+}
+
+
+
+?>
