959734cf1084e6375c19bb983645964b29837d42
ludovicdannhoff <ludovic.dannhoff@gmail.com>
2016-03-10 11:39:03 +0100

    Ajax fonctionnel

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,4 +5,41 @@ $('document').ready(function()
 		$('.shorter').toggleClass('col-sm-4').toggleClass('col-sm-3');
 		$('.panelticket').toggleClass('hidden');
 	});
+
+	// $.fn.scrollBottom = function() { 
+	//   return $(document).height() - this.scrollTop() - this.height(); 
+	// };
+
+	$(function(){
+		setInterval(function(){
+			$('#display-group').load('home.phtml #display-group > p');
+			$('#display-group').animate({scrollTop: $('#display-group')[0].scrollHeight});
+		}, 5000);
+	});
+
+	// window.onscroll = function() {myFunction()};
+
+	// function myFunction() {
+	// 	element = document.getElementById('display-group');
+	// 	console.log(element);
+	// 	element.scrollHeight - element.scrollTop === element.clientHeight;
+	//     console.log(element.clientHeight);
+	//     if (element.clientHeight === TRUE) {
+	// 		$(function(){
+	// 			setInterval(function(){
+	// 				$('#display-group').load('home.phtml #display-group > p');
+	// 				$('#display-group').animate({scrollTop: $('#display-group')[0].scrollHeight});
+	// 			}, 10000);
+	// 		});
+	//     } else {
+	// 		$(function(){
+	// 			setInterval(function(){
+	// 				$('#display-group').load('home.phtml #display-group > p');
+	// 				$('#display-group').animate({scrollTop: $('#display-group')[0].scrollHeight});
+	// 			}, 500);
+	// 		});
+	//     }
+	// }
+
+	// $("#element").scrollBottom();
 });
