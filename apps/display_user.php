bf04416be7d14ff831c4211354070be3e282191d
ludovicdannhoff <ludovic.dannhoff@gmail.com>
2016-03-11 14:20:54 +0100

    correction bug liste users

--- apps/display_user.php
+++ apps/display_user.php
@@ -1,13 +1,1 @@
-<?php
-$userManager = new userManager($db);
-$userManager->editDate($_SESSION['id']);
-$listUser = $userManager->getUserConnect();
-$count = 0;
-$max = sizeof($listUser);
-while ($count < $max)// On récupère les résultats de notre requête un par un
-{
-	$users = $listUser[$count];
-	require('views/display_user.phtml');
-	$count++;
-}
-?>
+<?php require('views/display_user.phtml') ?>
--- apps/user.php
+++ apps/user.php
@@ -1,1 +1,13 @@
-<?php require('views/user.phtml'); ?>
+<?php
+$userManager = new userManager($db);
+$userManager->editDate($_SESSION['id']);
+$listUser = $userManager->getUserConnect();
+$count = 0;
+$max = sizeof($listUser);
+while ($count < $max)
+{
+	$users = $listUser[$count];
+	require('views/user.phtml'); 
+	$count++;
+}
+?>
